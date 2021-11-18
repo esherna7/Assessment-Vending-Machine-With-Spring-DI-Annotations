8b49a336f3b31a814dd55ccc128b3ed74075d786
Esai Hernandez <hernandezesai123@gmail.com>
2021-11-18 12:05:35 -0600

    Added Vending Machine Project

---
+++ VendingMachine/src/main/java/com/esai/vendingmachine/App.java
@@ -0,0 +1,33 @@
+package com.esai.vendingmachine;
+
+import com.esai.vendingmachine.controller.VendingMachineController;
+import com.esai.vendingmachine.dao.ClassVendingMachineAuditDao;
+import com.esai.vendingmachine.dao.ClassVendingMachineAuditDaoFileImpl;
+import com.esai.vendingmachine.dao.ClassVendingMachineDaoException;
+import com.esai.vendingmachine.dao.ClassVendingMachinePersistenceException;
+import com.esai.vendingmachine.dao.VendingMachineDao;
+import com.esai.vendingmachine.dao.VendingMachineDaoFileImpl;
+import com.esai.vendingmachine.service.InsufficientFundsException;
+import com.esai.vendingmachine.service.NoItemInventoryException;
+import com.esai.vendingmachine.service.VendingMachineServiceLayer;
+import com.esai.vendingmachine.service.VendingMachineServiceLayerImpl;
+import com.esai.vendingmachine.ui.UserIO;
+import com.esai.vendingmachine.ui.UserIOConsoleImpl;
+import com.esai.vendingmachine.ui.VendingMachineView;
+
+/**
+ *
+ * @author Esai
+ */
+public class App {
+    
+    public static void main(String[] args) throws InsufficientFundsException, NoItemInventoryException, ClassVendingMachineDaoException, ClassVendingMachinePersistenceException{
+        UserIO myIO = new UserIOConsoleImpl();
+        VendingMachineView myView = new VendingMachineView(myIO);
+        VendingMachineDao myDao = new VendingMachineDaoFileImpl();
+        ClassVendingMachineAuditDao myAuditDao = new ClassVendingMachineAuditDaoFileImpl();
+        VendingMachineServiceLayer myService = new VendingMachineServiceLayerImpl(myDao, myAuditDao);
+        VendingMachineController controller = new VendingMachineController(myView, myService);
+        controller.run();
+    }
+}
